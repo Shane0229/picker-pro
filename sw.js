ca90eaed5b2fa91df28b3a1e6c671b8cf2588b23
Shane0229 <shane_0229@internode.on.net>
2026-08-19 21:15:23 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'picker-pro-shell-v2';
+const CACHE = 'picker-pro-shell-v3';
 const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
